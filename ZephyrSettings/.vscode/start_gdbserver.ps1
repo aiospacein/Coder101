9901661b6b0401469eeffea4e8258333dd0d894c
aiospacein <aiospaceindotcom@gmail.com>
2025-12-29 18:13:03 +0530

    ZEPHYR DEGUS SETTINg FINALIZED

---
+++ ZephyrSettings/.vscode/start_gdbserver.ps1
@@ -0,0 +1,18 @@
+# Kill any existing PEGDBServer
+Get-Process pegdbserver_console -ErrorAction SilentlyContinue | Stop-Process -Force
+
+# Small delay to ensure clean shutdown
+Start-Sleep -Milliseconds 500
+
+# Start PEGDBServer
+Start-Process `
+  "C:\PEMicro\PEDrivers\com.pemicro.debug.gdbjtag\win32\pegdbserver_console.exe" `
+  -ArgumentList `
+    "-startserver",
+    "-device=NXP_S32K3xx_S32K344",
+    "-interface=USBMULTILINK",
+    "-port=USB1",
+    "-gdbport=3333" `
+  -WindowStyle Hidden
+
+Write-Host "PEGDBServer started on port 3333"
